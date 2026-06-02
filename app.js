d55ebf44a035f96fc98a6cb478d9832311357d6b
sakunimama <ronaldteron888@gmail.com>
2026-06-02 17:50:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log("Hyperwork Loaded");
+function showPage(page){
+
+    document.getElementById("dashboardPage").style.display = "none";
+    document.getElementById("settingsPage").style.display = "none";
+
+    if(page === "dashboard"){
+        document.getElementById("dashboardPage").style.display = "block";
+    }
+
+    if(page === "settings"){
+        document.getElementById("settingsPage").style.display = "block";
+    }
+
+}
+
+function saveSettings(){
+
+    let settings = {
+        officeName: document.getElementById("officeName").value,
+        officeCode: document.getElementById("officeCode").value
+    };
+
+    localStorage.setItem(
+        "hyperworkSettings",
+        JSON.stringify(settings)
+    );
+
+    alert("Settings Saved");
+
+}
+
+window.onload = function(){
+
+    let data = localStorage.getItem("hyperworkSettings");
+
+    if(data){
+
+        let settings = JSON.parse(data);
+
+        document.getElementById("officeName").value =
+            settings.officeName || "";
+
+        document.getElementById("officeCode").value =
+            settings.officeCode || "";
+    }
+
+}
