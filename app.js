6f682ffbd2ecc78b587acc384221e2ed32e5a059
sakunimama <ronaldteron888@gmail.com>
2026-06-08 13:54:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,3 +52,100 @@ function showPage(pageId){
     .classList.add("active-content");
 
 }
+let documents = JSON.parse(
+localStorage.getItem("documents")
+) || [];
+
+function addDocument(){
+
+    let number =
+    document.getElementById("docNumber").value;
+
+    let subject =
+    document.getElementById("docSubject").value;
+
+    let from =
+    document.getElementById("docFrom").value;
+
+    if(number === "" || subject === ""){
+        alert("Fill required fields");
+        return;
+    }
+
+    documents.push({
+        number,
+        subject,
+        from
+    });
+
+    localStorage.setItem(
+        "documents",
+        JSON.stringify(documents)
+    );
+
+    renderDocuments();
+
+    document.getElementById("docNumber").value="";
+    document.getElementById("docSubject").value="";
+    document.getElementById("docFrom").value="";
+}
+
+function renderDocuments(){
+
+    let tbody =
+    document.querySelector(
+        "#documentTable tbody"
+    );
+
+    tbody.innerHTML="";
+
+    documents.forEach((doc,index)=>{
+
+        tbody.innerHTML += `
+        <tr>
+
+            <td>${index+1}</td>
+
+            <td>${doc.number}</td>
+
+            <td>${doc.subject}</td>
+
+            <td>${doc.from}</td>
+
+            <td>
+
+                <button onclick="deleteDocument(${index})">
+                    Delete
+                </button>
+
+            </td>
+
+        </tr>
+        `;
+
+    });
+
+}
+
+function deleteDocument(index){
+
+    if(confirm("Delete document?")){
+
+        documents.splice(index,1);
+
+        localStorage.setItem(
+            "documents",
+            JSON.stringify(documents)
+        );
+
+        renderDocuments();
+
+    }
+
+}
+
+window.onload = function(){
+
+    renderDocuments();
+
+};
